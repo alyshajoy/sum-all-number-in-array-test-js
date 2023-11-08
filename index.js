a6a08fad9a5cf15ba7db02786a2c795bd022abb1
Alysha Huot <alyshajhuot@gmail.com>
2023-11-08 15:23:14 -0700

    complete sum recursion function

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
-}
+
+  let sum = 0;
+
+  for (let item of array) {
+    if (Array.isArray(item)) {
+      sum += sumItems(item);
+    } else {
+      sum += item;
+    }
+  }
+
+  return sum;
+};
+
+console.log(sumItems([[1, 2, [[3], 4]], 5, []]));
+console.log(sumItems([[[[[[[[[[[[[1]]]]]]]]]]]]]));
 
 module.exports = sumItems;
+
+
+// [1, 2, 3, 4, 5] ->	15
+// [[1, 2, [[3], 4]], 5, []] ->	15
+// [[[[[[[[[[[[[1]]]]]]]]]]]]]	-> 1
+
+// base case: 
